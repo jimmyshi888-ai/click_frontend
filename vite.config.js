9112f24cb54db8e08e975add44164375407a2da2
jimmyshi888-ai <jimmyshi888@gmail.com>
2026-01-09 21:55:41 +0800

    正確名稱

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // ★★★ 這裡一定要是 '/click_frontend/' ★★★
+  // 如果你寫成 '/click-frontend/' (連字號) 或者 '/' 都會導致 404
+  base: '/click_frontend/', 
+
   plugins: [
     vue(),
   ],
